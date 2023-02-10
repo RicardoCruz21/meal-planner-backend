8c4b38893853214a83bf9e247e8978e07b192551
RicardoCruz21 <abstergo21@gmail.com>
2023-02-10 12:29:50 -0600

    create rest controller for ingredients

---
+++ src/main/java/com/techelevator/controller/IngredientController.java
@@ -0,0 +1,29 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.IngredientDao;
+import com.techelevator.model.meal.Ingredient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class IngredientController {
+
+    private IngredientDao ingredientDao;
+
+    public IngredientController(IngredientDao ingredientDao) {
+        this.ingredientDao = ingredientDao;
+    }
+
+    @GetMapping("/ingredients")
+    public List<Ingredient> getIngredients() {
+        return ingredientDao.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/ingredients")
+    public int addNewIngredient(@RequestBody String ingredient) {
+        return ingredientDao.create(ingredient);
+    }
+}
